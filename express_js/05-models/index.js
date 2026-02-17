cc6953fdc38cce6493471596582fc636af7dcd6e
Manoj Rawat <manojrawat87634@gmail.com>
2026-02-17 16:25:36 +0530

    inital comit

--- express_js/05-models/index.js
+++ express_js/05-models/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import StudentModel from "./src/models/student.model.js";
 import connectionToDatabase from "./src/db/connection.js";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors({origin : "*"}));
 app.get('/user', async(req, res)=> {
     const data = await StudentModel.find();
     return res.json(data);
